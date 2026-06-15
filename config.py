6183d74a4a69c40eca784dec9d018091fa6d6148
abimanim311-ctrl <abimanim311@gmail.com>
2026-06-15 16:14:20 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    MYSQL_HOST = 'plant-green-inertia-abimani27112003-3e8c.g.aivencloud.com'
+    MYSQL_PORT = 23879
+    MYSQL_USER = 'avnadmin'
+    MYSQL_PASSWORD = 'AVNS_twqHLYh1gu_eg85QPV-'
+    MYSQL_DB = 'defaultdb'
